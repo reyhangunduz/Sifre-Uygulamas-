4238c4b4e1606a80919857653d3ba86d3885d684
reyhangunduz <144460500+reyhangunduz@users.noreply.github.com>
2023-09-28 13:58:38 +0300

    Sifre Kontrol Uygulaması
    Belirli uzunluğa sahip, büyük ve küçük harfe duyarlı ve özel karakterlere sahip olma durumlarına göre değerlendirme yapan bir şifre kontrol uygulaması

---
+++ SifreKontrolUygulamasi/SifreKontrolUygulamasi/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace SifreKontrolUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtSifreAlan = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(209, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Şifre Kontrol Programı";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(237, 148);
+            label2.Name = "label2";
+            label2.Size = new Size(10, 15);
+            label2.TabIndex = 1;
+            label2.Text = " ";
+            // 
+            // txtSifreAlan
+            // 
+            txtSifreAlan.Location = new Point(168, 97);
+            txtSifreAlan.Name = "txtSifreAlan";
+            txtSifreAlan.Size = new Size(198, 23);
+            txtSifreAlan.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(518, 278);
+            Controls.Add(txtSifreAlan);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtSifreAlan;
+    }
+}
---
+++ SifreKontrolUygulamasi/SifreKontrolUygulamasi/Form1.cs
@@ -0,0 +1,27 @@
+﻿namespace SifreKontrolUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void KontrolEtButton_Click(object sender, EventArgs e)
+        {
+            string girilenSifre = txtSifreAlan.Text;
+
+            // Şifre kontrolü burada gerçekleştirilebilir.
+            string dogruSifre = "gizli_sifre";
+
+            if (girilenSifre == dogruSifre)
+            {
+                MessageBox.Show("Şifre doğru!", "Başarılı");
+            }
+            else
+            {
+                MessageBox.Show("Şifre yanlış!", "Hata");
+            }
+        }
+
+    }
+}
